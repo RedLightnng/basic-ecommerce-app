b4b44928942d3e25f6082cef60bf60302623d326
yogya <yogyabansal8@gmail.com>
2024-10-01 18:48:16 +0530

    initial commit for basic ecommerce app

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:basic_ecommerce_app/pages/intro_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Material App', home: IntroPage());
+  }
+}
